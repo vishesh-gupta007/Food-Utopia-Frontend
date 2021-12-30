28ee191a47547adf7771da0c947bbab9df529909
vishesh-gupta007 <f20200352@goa.bits-pilani.ac.in>
2021-12-30 22:34:56 +0530

    added manual heroku

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
     setIsPending(true);
 
-    fetch("/user", {
+    fetch("https://food-utopia.herokuapp.com/user", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
